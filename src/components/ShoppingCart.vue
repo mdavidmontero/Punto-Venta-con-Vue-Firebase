850e6d810c8d6af60268c061f3a13d8753a8a3ee
mdavidrodriguez <mdavidrodriguez@unicesar.edu.co>
2024-01-30 12:12:29 -0500

    Terminando carrito de compras

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { useCartStore } from '@/stores/cart';
+import Amount from './Amout.vue'
 import ShoppingCartItem from './ShoppingCartItem.vue'
+import { formatCurrency } from '@/helpers';
 const cart = useCartStore()
 </script>
 <template>
@@ -9,8 +11,21 @@ const cart = useCartStore()
         <p class="text-4xl font-bold text-gray-900">Resumen de Venta</p>
         <ul role="list" class="mt-6 divide-y divide-gray-200">
             <ShoppingCartItem v-for="item in cart.items" :key="item.id" :item="item" />
-
         </ul>
+        <dl class="space-y-6 border-t border-gray-200 pt-6 text-sm font-medium text-gray-500">
+            <Amount>
+                <template #label>Subtotal: </template>
+                {{ formatCurrency(cart.subtotal) }}
+            </Amount>
+            <Amount>
+                <template #label>Impuestos: </template>
+                {{ formatCurrency(cart.taxes) }}
+            </Amount>
+            <Amount>
+                <template #label>Total a Pagar: </template>
+                {{ formatCurrency(cart.total) }}
+            </Amount>
+        </dl>
     </div>
 </template>
 
