758f2a976ebfedacdbb6e8448f1e0f5afb84fe60
mdavidrodriguez <mdavidrodriguez@unicesar.edu.co>
2024-01-29 22:25:34 -0500

    Creando routing, configurando formkit, firebase-vuefire

---
+++ src/components/Logo.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1 class="text-3xl font-black text-white">Punto de <span class="text-green-400">Venta</span></h1>
+</template>
+
+    
---
+++ src/views/admin/SalesView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+<template>
+    <div>
+        <h1 class="text-4xl font-black my-4">Resumen de Ventas</h1>
+    </div>
+</template>
+
